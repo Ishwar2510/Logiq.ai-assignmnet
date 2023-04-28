e9525d4a18283b8c81eb12c4be9015958ec11025
Ishwar2510 <ishwar2510@gmail.com>
2023-04-28 20:24:26 +0530

    added mutex

--- cache/cache.go
+++ cache/cache.go
@@ -3,6 +3,8 @@ package cache
 import (
 	"errors"
 	"time"
+	"sync"
+	
 )
 
 var (
@@ -11,9 +13,11 @@ var (
 	ErrMaxLimitReached = errors.New("maximum cache limit reached")
 )
 
+
 type inMemCache struct {
 	maxSize int
 	cache   map[string]cacheObject
+	mu *sync.RWMutex 
 }
 
 type cacheObject struct {
@@ -32,66 +36,111 @@ func NewCache(maxSize int) Cache {
 	return &inMemCache{
 		maxSize: maxSize,
 		cache:   make(map[string]cacheObject),
+		mu: &sync.RWMutex{},
 	}
 }
 
 func (c *inMemCache) Add(key string, value interface{}, expiration time.Duration) error {
-	if c.maxSize == len(c.cache) {
+	
+
+	if c.isFull(){
 		c.remove()
-		
 	}
-	_, err := c.get(key)
-	if err == nil {
+	_,expired,err:=c.get(key)
+	if err == nil && !expired {
 		return ErrAlreadyExists
 	}
+
+	 if expired {
+		c.delete(key)
+	}
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.cache[key] = cacheObject{
 		value:      value,
 		expiration: time.Now().Add(expiration),
 	}
 	return nil
 }
-func (c *inMemCache) remove() {
+
+func (c *inMemCache) isFull() bool{
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	if c.maxSize > len(c.cache) {
+		return false 
+	}
+	return true
+}
+
+func (c *inMemCache) remove()  {
+	
 	now := time.Now()
 	var oldestKey string
 	var oldestTime time.Time
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	for key, obj := range c.cache {
 		if obj.expiration.Before(now) {
 			delete(c.cache, key)
-			return
+			return 
 		}
 		if obj.expiration.Before(oldestTime) {
 			oldestTime = obj.expiration
 			oldestKey = key
 		}
 	}
-	delete(c.cache, oldestKey)
+	
+		delete(c.cache, oldestKey)
+	
+	
 }
 
-func (c *inMemCache) get(key string) (interface{}, error) {
+func (c *inMemCache) get(key string) (interface{},bool, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
 	obj, isPresent := c.cache[key]
 	if !isPresent {
-		return nil, ErrNotFound
+		return nil, false,ErrNotFound
 	}
 	if obj.expiration.Before(time.Now()) {
-		delete(c.cache, key)
-		return nil, ErrNotFound
+		return nil,true, nil
 	}
-	return obj.value, nil
+	return obj.value, false,nil
 }
 
 func (c *inMemCache) Get(key string) (interface{}, error) {
-	return c.get(key)
+	 value,expired,err:=c.get(key)
+	 if err!=nil{
+		return nil,err
+	 }
+
+	 if expired {
+		c.delete(key)
+		return nil,ErrNotFound
+	 }
+	return value,nil
 }
 
+func (c *inMemCache) delete(key string){
+		c.mu.Lock()
+		defer c.mu.Unlock()
+		delete(c.cache,key)
+} 
 func (c *inMemCache) MaxSize(size int) {
 	c.maxSize = size
 }
 
 func (c *inMemCache) Delete(key string) error {
-	_, err := c.get(key)
-	if err != nil {
+	_,expired,err:=c.get(key)
+	 if err!=nil{
 		return err
-	}
-	delete(c.cache, key)
+	 }
+
+	 if expired {
+		c.delete(key)
+		return ErrNotFound
+	 }
+	 c.delete(key)
 	return nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/Ishwar2510/Logiq.ai-assignmnet/api"
+	
 	"log"
 	"net/http"
+
+	"github.com/Ishwar2510/Logiq.ai-assignmnet/api"
 
 	"github.com/Ishwar2510/Logiq.ai-assignmnet/cache"
 	"github.com/gorilla/mux"
@@ -17,8 +19,8 @@ func main() {
 	router.HandleFunc("/cache", api.Add).Methods(http.MethodPost)
 	router.HandleFunc("/cache/{key}", api.Get).Methods(http.MethodGet)
 	router.HandleFunc("/cache/{key}", api.Delete).Methods(http.MethodDelete)
+	
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal(err)
 	}
-	
 }
